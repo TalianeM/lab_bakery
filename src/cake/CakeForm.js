b7b7f74e70aa880333948c092b87230aaa93aec5
Taliane Moussassa <talianegloire392@gmail.com>
2023-05-18 08:38:28 +0100

    adds CakeForm class

---
+++ src/cake/CakeForm.js
@@ -0,0 +1,51 @@
+
+ 
+//  (cakeForm) 
+
+
+
+// import {useState} 
+
+// const CakeForm = ({addNewDetail}) => {
+
+//     // useState is ("") rather than (0), as cakeName, rating and ingredients are String
+
+//     const [cakeName, setCakeName] = useState("");  
+//     const [cakeRating, setRating] = useState("");
+//     const [CakeIngredients, setIngredients] = useState("");
+
+
+
+//     const handListSubmit = (e) => { 
+//         e.preventDefault();
+//     }
+
+//     const handleFormSubmit = {
+//         const: newIngredientsList,
+//         cakeName : cakeName,
+//         cakeRating : cakeRating,
+//         CakeIngredients : CakeIngredients,
+
+//     }
+    
+//     addNewDetail(newIngredientsList);
+
+
+//     // return (
+//     //     <>
+//     //       <form onSubmit={handleFormSubmit}>
+//     //         <input 
+//     //           type="text"
+//     //           name="cakeName"
+//     //           placeholder="Cake Name"
+//     //           value={cakeName}
+//     //           onChange={(e) => setCakeName(e.target.value)}/>
+//     //       </form>
+          
+//     //       <p>{error}</p>
+//     //     </>
+//     //   );
+//     // };
+
+
+// export default CakeForm // Extension (cakeForm)
